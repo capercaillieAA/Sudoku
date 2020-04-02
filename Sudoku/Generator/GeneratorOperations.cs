709de4457a26bd081ca8ebb934e4788182f940a9
Andruxa <andruxaglushkov@yandex.ru>
2020-04-03 01:40:12 +0300

    added sudoku generator class

--- Sudoku/Generator/GeneratorOperations.cs
+++ Sudoku/Generator/GeneratorOperations.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using static System.Math;
 
-namespace Sudoku.Classes
+namespace Sudoku
 {
     static class GeneratorOperations
     {
         static Random rand = new Random();
 
-        static public void TransposeMatrix(ref byte[,] table)
+        static public void TransposeMatrix(byte[,] table)
         {
             for (int i = 0; i < table.GetLength(0); i++)
-                for (int j = 0; j < table.GetLength(1); j++)
+                for (int j = i; j < table.GetLength(1); j++)
                     Swap(ref table[i, j], ref table[j, i]);
         }
 
-        static public void SwapColumnsInBlock(ref byte[,] table)
+        static public void SwapColumnsInBlock(byte[,] table)
         {
             int dim = (int)Sqrt(table.GetLength(0));
             var startColumn = rand.Next(0, dim) * dim;
@@ -27,14 +27,14 @@ namespace Sudoku.Classes
                 Swap(ref table[i, randColumn1], ref table[i, randColumn2]);
         }
 
-        static public void SwapRowsInBlock(ref byte[,] table)
+        static public void SwapRowsInBlock(byte[,] table)
         {
-            TransposeMatrix(ref table);
-            SwapColumnsInBlock(ref table);
-            TransposeMatrix(ref table);
+            TransposeMatrix(table);
+            SwapColumnsInBlock(table);
+            TransposeMatrix(table);
         }
 
-        static public void SwapBlockColumns(ref byte[,] table)
+        static public void SwapBlockColumns(byte[,] table)
         {
             int dim = (int)Sqrt(table.GetLength(0));
             int randBlock1 = rand.Next(0, dim);
@@ -45,11 +45,11 @@ namespace Sudoku.Classes
                     Swap(ref table[i, randBlock1 * dim + j], ref table[i, randBlock2 * dim + j]);
         }
 
-        static public void SwapBlockRows(ref byte[,] table)
+        static public void SwapBlockRows(byte[,] table)
         {
-            TransposeMatrix(ref table);
-            SwapBlockColumns(ref table);
-            TransposeMatrix(ref table);
+            TransposeMatrix(table);
+            SwapBlockColumns(table);
+            TransposeMatrix(table);
         }
 
         static void Swap(ref byte a, ref byte b)
---
+++ Sudoku/Generator/SudokuGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Math;
+
+namespace Sudoku
+{
+    static class SudokuGenerator
+    {
+        static Random rand = new Random();
+
+        static public void Generate(byte[,] table)
+        {
+            Init(table);
+            Mix(table);
+        }
+
+        static void Init(byte[,] table)
+        {
+            int size = table.GetLength(0);
+            int dim = (int)Sqrt(size);
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    table[i, j] = (byte)((i * dim + i / dim + j) % (size) + 1);
+        }
+
+        static void Mix(byte[,] table)
+        {
+            const int lowerMixer = 20000;
+            const int upperMixer = 30000;
+
+            var methods = typeof(GeneratorOperations).GetMethods();
+            int randValue = rand.Next(lowerMixer, upperMixer);
+            for (int i = 0; i < randValue; i++)
+            {
+                int randMethod = rand.Next(methods.Length - 4);
+                methods[randMethod].Invoke(null, new object[] { table });
+            }
+        }
+    }
+}
