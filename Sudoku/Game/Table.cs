9e4c30a045c24326a31ca48a4bdeec60c87eb480
Andruxa <andruxaglushkov@yandex.ru>
2020-04-03 16:53:34 +0300

    added a class of sudoku table

---
+++ Sudoku/Game/Table.cs
@@ -0,0 +1,19 @@
+﻿namespace Sudoku
+{
+    class Table
+    {
+        private readonly int size;
+        private readonly byte[,] table;
+
+        public Table(int dim)
+        {
+            size = dim * dim;
+            table = new byte[size, size];
+        }
+
+        public void Create(Difficult difficult)
+        {
+            SudokuGenerator.Generate(table, size, difficult);
+        }
+    }
+}
