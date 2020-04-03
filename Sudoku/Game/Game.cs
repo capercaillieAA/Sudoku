b1cc08a8dc54574c815546c9d18a34c9c6bf0b38
Andruxa <andruxaglushkov@yandex.ru>
2020-04-03 17:03:27 +0300

    added a game class

---
+++ Sudoku/Game/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    class Game
+    {
+        private Table table;
+        private Settings settings;
+
+        public void Start()
+        {
+            settings = new Settings().GetSettings();
+            table = new Table(settings.Dimension);
+            table.Create(settings.Difficult);
+        }
+    }
+}
