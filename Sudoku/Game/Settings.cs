022225d97b5ae94a066d0f41b9d65c49d2f5afbf
Andruxa <andruxaglushkov@yandex.ru>
2020-04-03 16:47:24 +0300

    added a class of game settings

---
+++ Sudoku/Game/Settings.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace Sudoku
+{
+    enum Difficult
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+
+    class Settings
+    {
+        public Difficult Difficult { get; }
+
+        public int Dimension { get; }
+
+        public bool Hints { get; }
+
+        public bool Timer { get; }
+
+        public int SoundVolume { get; }
+
+        public void Save()
+        {
+            using (var output = new StreamWriter("settings.json"))
+                output.WriteLine(JsonConvert.SerializeObject(this));
+        }
+
+        public Settings GetSettings()
+        {
+            using (var input = new StreamReader("settings.json"))
+                return JsonConvert.DeserializeObject<Settings>(input.ReadToEnd());
+        }
+    }
+}
